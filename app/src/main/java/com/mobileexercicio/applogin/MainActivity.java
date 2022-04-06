d3ccaf497ff0d82999ce29d8e4467c5526a3617a
magela-lopez <71371549+magela-lopez@users.noreply.github.com>
2022-04-06 04:00:30 -0300

    First Commit

---
+++ app/src/main/java/com/mobileexercicio/applogin/MainActivity.java
@@ -0,0 +1,47 @@
+package com.mobileexercicio.applogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String user = "Magela";
+    String password = "magela22";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText usuario = findViewById(R.id.txtUser);
+        EditText senha = findViewById(R.id.txtPass);
+        Button btnEntrar = findViewById(R.id.btnLogin);
+        
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(user.equals(usuario.getText().toString()) & password.equals(senha.getText().toString())){
+                    Toast.makeText( getApplicationContext(), "Login feito com sucesso", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("NomeUsuario", usuario.getText().toString());
+                    intent.putExtras(bundle);
+
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText( getApplicationContext(), "Usuario ou senha incorretos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        
+    }
+}
